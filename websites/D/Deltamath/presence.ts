74597a194e57601a7d6627896ef6e434fa13f9d7
Ling <39093500+ling-nyc@users.noreply.github.com>
2022-08-21 14:29:09 -0400

    Update presence.ts

--- websites/D/Deltamath/presence.ts
+++ websites/D/Deltamath/presence.ts
@@ -4,45 +4,45 @@ const presence = new Presence({
   browsingTimestamp = Math.floor(Date.now() / 1000);
 let percentage: HTMLElement, assignment: string | undefined;
 let totalProblems: HTMLElement, current: HTMLElement, totalPercentCorrect: HTMLElement;
-let totalProblemsString: string, currentString: string, totalPercentCorrectString: string;
+
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "deltamath"
   };
-  // Theres no presence for the homepage because you would really only be working on an assignment 90% of the time
+
+  function updateTimestamp() {
+    delete presenceData.startTimestamp;
+    presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+  }
+  // Working on an assignment
   if (document.location.pathname.includes("/solve")) {
     percentage = document.querySelector(
       "body > app-root > student > div.main.container > problem > problem-toolbar > div.col-md-10.col-lg-9.student-progress.paper-shadow > span.complete-area > span"
     );
-    delete presenceData.startTimestamp;
-    // For if you want to reset the timer on every problem comment/uncomment above line
-    presenceData.startTimestamp = Math.floor(Date.now() / 1000);
-    assignment = document
-      .querySelector<HTMLElement>(
-        "#question_page > div > div > div.problem-header > div:nth-child(1)"
-      )
-      ?.innerHTML?.split(/<br.*>/g)[1];
+    updateTimestamp()
+    assignment = document.querySelector("#question_page > div > div > div.problem-header > div:nth-child(1)")?.innerHTML?.split(/<br.*>/g)[1];
     presenceData.details = assignment;
     presenceData.state = percentage.textContent;
   } 
-  
+  // Statistics page
   if (document.location.pathname.includes("/history")){
     totalProblems = document.querySelector("body > app-root > student > div.main.container > student-detail > div > div.lead > span:nth-child(1)")
     totalPercentCorrect = document.querySelector("body > app-root > student > div.main.container > student-detail > div > div.lead > span:nth-child(3)");
-    delete presenceData.startTimestamp;
-    // For if you want to reset the timer on every page comment/uncomment above line
-    presenceData.startTimestamp = Math.floor(Date.now() / 1000);
-    totalProblemsString = totalProblems.textContent
-    totalPercentCorrectString = totalPercentCorrect.textContent
-    presenceData.details = totalProblemsString.substring(totalProblemsString.length-3, totalProblemsString.length) + " completed, " + totalPercentCorrectString.substring(totalPercentCorrectString.length, totalPercentCorrectString.length-5) + " correct.";
+    updateTimestamp()
+    presenceData.details = totalProblems.textContent.substring(totalProblems.innerText.length-3, totalProblems.textContent.length) + " completed, " + totalPercentCorrect.textContent.substring(totalPercentCorrect.textContent.length, totalPercentCorrect.textContent.length-5) + " correct.";
     presenceData.state = "Viewing Statistics" ;
   }
+  // Settings page
   if (document.location.pathname.includes("/info")){
+    delete presenceData.state;
     presenceData.details = "Viewing Settings";
+    updateTimestamp()
   }
+  //Miscelaneous pages not covered in other if statements
   else {
     presenceData.details = "Browsing site";
-    presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+    delete presenceData.state;
+    updateTimestamp();
   }
   if (presenceData.details) presence.setActivity(presenceData);
   else presence.setActivity();
